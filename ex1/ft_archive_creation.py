7b0f09c2b40d6d7c2f583ad1d8afd336924d502f
trakotos <trakotos@student.42antananarivo.mg>
2026-04-03 11:33:33 +0300

    add ft_archive_creation.py: implement file reading and writing functionality

--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -2,12 +2,12 @@
 # ########################################################################### #
 #   shebang: 1                                                                #
 #                                                          :::      ::::::::  #
-#   ft_ancient_text.py                                   :+:      :+:    :+:  #
+#   ft_archive_creation.py                               :+:      :+:    :+:  #
 #                                                      +:+ +:+         +:+    #
 #   By: trakotos <trakotos@student.42antananarivo.   +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
-#   Created: 2026/04/03 10:27:05 by trakotos            #+#    #+#            #
-#   Updated: 2026/04/03 11:32:03 by trakotos           ###   ########.fr      #
+#   Created: 2026/04/03 11:04:39 by trakotos            #+#    #+#            #
+#   Updated: 2026/04/03 11:31:58 by trakotos           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
@@ -15,7 +15,7 @@ import sys
 import typing
 
 
-def print_content_file() -> None:
+def read_content_file() -> str:
     if len(sys.argv) != 2:
         raise Exception(f"Usage: {sys.argv[0]} <file>")
     file_name = sys.argv[1]
@@ -23,19 +23,44 @@ def print_content_file() -> None:
     f: typing.TextIO | None = None
     try:
         f = open(file_name, "r")
+        contents = f.read()
         print("---\n")
-        print(f.read())
+        print(contents)
         print("---")
+        new_contents = contents.replace("\n", "#\n")
     except Exception as e:
         raise Exception(f"Error opening file '{file_name}': {e}")
     finally:
         if f is not None:
             print(f"File '{file_name}' closed.")
             f.close()
+    return new_contents
+
+
+def write_content(contents: str):
+    print("\nTransform data:\n---\n")
+    print(contents)
+    print("---")
+    file_name = input("Enter new file name (or empty): ")
+    if file_name != "":
+        print(f"Saving data to '{file_name}'")
+        f: typing.TextIO | None = None
+        try:
+            f = open(file_name, "w")
+            print(contents, file=f)
+            print(f"Data saved in file '{file_name}'.")
+        except Exception as e:
+            print(e)
+        finally:
+            if f is not None:
+                f.close()
+    else:
+        print("Not saving data.")
 
 
 if __name__ == "__main__":
     try:
-        print_content_file()
+        contents = read_content_file()
+        write_content(contents)
     except Exception as e:
         print(e)
